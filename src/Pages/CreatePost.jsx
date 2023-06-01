aad5bfd5658dbe782eb159b2b1c7f347bf3d145c
ijeoma Anyanwu <anyanwu.ijeomajovita@gma>
2023-06-01 12:45:45 +0100

    client

--- src/Pages/CreatePost.jsx
+++ src/Pages/CreatePost.jsx
@@ -16,10 +16,65 @@ const CreatePost = () => {
   const [generatingImg, setGeneratingImg] = useState(false);
   const [loading, setLoading] = useState(false);
 
-  const generateImage = () => {};
-  const handleSubmit = () => {};
-  const handleChange = () => {};
-  const handleSurpriseMe = () => {};
+  const generateImage = async() => {
+    if (form.prompt) {
+      try {
+        setGeneratingImg(true);
+        const response = await fetch("https://open-ai-mi14.onrender.com/api/v1/dalle", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ prompt: form.prompt }),
+        });
+
+        const data = await response.json();
+        setForm({ ...form, photo: `data:image/jpeg;base64,${data.photo}` });
+      } catch (err) {
+        alert(err);
+      } finally {
+        setGeneratingImg(false);
+      }
+    } else {
+      alert("please enter prompt");
+    }
+  };
+  const handleSubmit = async(e) => {
+    e.preventDefault()
+
+    if(form.prompt && form.photo){
+      setLoading(true);
+
+      try{
+        const response =  await fetch('https://open-ai-mi14.onrender.com/api/v1/post', {
+          method:'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(form)
+          
+        })
+        await response.json();
+        navigate('/')
+      }catch(err){
+        alert(err)
+      }finally{
+        setLoading(false)
+      }
+
+
+    }else{
+      alert('please enter a prompt and generate an Image')
+    }
+
+  };
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+  const handleSurpriseMe = () => {
+    const randomPrompt = getRandomPrompt(form.prompt);
+    setForm({ ...form, prompt: randomPrompt });
+  };
 
   return (
     <section className="max-w">
@@ -27,7 +82,7 @@ const CreatePost = () => {
         <h1 className="font-extrabold text-[#222328] text-[32px]"> Create</h1>
         <p className="mt-2 text-[#666e75] text-[16px] max-w [500px]">
           Create Imaginative and visually stunning images through DALL_E AI and
-          share them with the communiy
+          share them with the community
         </p>
       </div>
       <form className="mt-16 max-w-3xl" onSubmit={handleSubmit}>
@@ -83,7 +138,16 @@ const CreatePost = () => {
           </button>
         </div>
         <div className="mt-10">
-              <p className="">Once you have created the image you want, you can share it with others in the community</p>
+          <p className="mt-2 text-[#666e75] text-[14px]">
+            Once you have created the image you want, you can share it with
+            others in the community
+          </p>
+          <button
+            type="Submit"
+            className="mt-3 text-white bg-[#6469ff] font-meduim rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+          >
+            {loading ? "Sharing..." : "share with the community"}
+          </button>
         </div>
       </form>
     </section>
